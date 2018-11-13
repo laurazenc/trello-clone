378683dccadeb95ee57946d9b2ce8fea947669e6
Laura <laurazenc@gmail.com>
2018-11-14 00:30:45 +0100

    Test config and helpers

---
+++ server/src/utils/tests/helper.js
@@ -0,0 +1,41 @@
+import mongoose from "mongoose";
+process.env.NODE_ENV = "test";
+
+require("dotenv").config();
+
+const dbUri = `mongodb://localhost:27017/${process.env.DATABASE_URI_TEST}`;
+
+console.log("dbUri", dbUri);
+
+export async function connectMongoose() {
+  jest.setTimeout(20000);
+  return mongoose.connect(
+    dbUri,
+    { useNewUrlParser: true, useCreateIndex: true }
+  );
+}
+
+export async function clearDatabase() {
+  await mongoose.connection.db.dropDatabase();
+}
+
+export async function disconnectMongoose() {
+  await mongoose.disconnect();
+  mongoose.connections.forEach(connection => {
+    const modelNames = Object.keys(connection.models);
+
+    modelNames.forEach(modelName => {
+      delete connection.models[modelName];
+    });
+
+    const collectionNames = Object.keys(connection.collections);
+    collectionNames.forEach(collectionName => {
+      delete connection.collections[collectionName];
+    });
+  });
+
+  const modelSchemaNames = Object.keys(mongoose.modelSchemas);
+  modelSchemaNames.forEach(modelSchemaName => {
+    delete mongoose.modelSchemas[modelSchemaName];
+  });
+}
