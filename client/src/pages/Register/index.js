862e14b754780d4b927e1fd750190df67cb046ff
Laura <laurazenc@gmail.com>
2018-11-15 09:30:22 +0100

    Register page and view

--- client/src/pages/Register/index.js
+++ client/src/pages/Register/index.js
@@ -1,7 +1,67 @@
 import React, { Component } from "react";
+import { Mutation, withApollo } from "react-apollo";
+import gql from "graphql-tag";
 
-export default class Register extends Component {
+import withFullScreenLayout from "./../../layouts/FullScreenLayout";
+import RegisterView from "./RegisterView";
+
+const REGISTER_MUTATION = gql`
+  mutation REGISTER_MUTATION(
+    $email: String!
+    $displayName: String!
+    $password: String!
+    $confirmPassword: String!
+  ) {
+    register(
+      email: $email
+      displayName: $displayName
+      password: $password
+      confirmPassword: $confirmPassword
+    ) {
+      errors {
+        path
+        message
+      }
+      result
+    }
+  }
+`;
+
+class Register extends Component {
+  onFinish = values => {
+    const {
+      data: {
+        register: { errors }
+      }
+    } = values;
+
+    const {
+      history,
+      location: { state },
+      client
+    } = this.props;
+
+    client.resetStore();
+
+    if (!errors) history.push(state ? state.next : "/");
+  };
   render() {
-    return <div>Register</div>;
+    return (
+      <Mutation mutation={REGISTER_MUTATION}>
+        {(register, { loading, error, data }) => {
+          return (
+            <RegisterView
+              onFinish={this.onFinish}
+              register={register}
+              error={error}
+              data={data}
+              loading={loading}
+            />
+          );
+        }}
+      </Mutation>
+    );
   }
 }
+
+export default withFullScreenLayout(withApollo(Register));
