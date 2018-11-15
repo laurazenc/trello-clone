0c2971bebd5bac8e19ebafca5460b0b27410f9ed
Laura <laurazenc@gmail.com>
2018-11-15 16:59:11 +0100

    New messages

--- client/src/utils/validations/messages/userMessages.js
+++ client/src/utils/validations/messages/userMessages.js
@@ -10,5 +10,13 @@ export const userMessages = {
   invalidLogin: "Invalid login",
   confirmEmail:
     "Congrats you have successfully register! Please, check your email to confirm your account.",
-  confirmedAccount: "Your email has been confirmed!"
+  confirmedAccount: "Your email has been confirmed!",
+  forgotPasswordEmail: "Which is the email of your Trello clone account?",
+  checkEmailAccount:
+    "Well...something wen't wrong. \nPlease check that the email you submited is the one you used in your Trello clone app account",
+  forgotPasswordEmailSent: "We've sent an email to change you password.",
+  changeYourPassword: "Choose your new password",
+  changePasswordProblem: "Well...something wen't wrong. <br>Please try again",
+  passwordUpdated:
+    "Hooray! Your password has been updated. Try login with the new password."
 };
