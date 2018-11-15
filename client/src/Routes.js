dbfb1e02e6a6b36828a5796a8f995da4f5617813
Laura <laurazenc@gmail.com>
2018-11-15 16:59:28 +0100

    Forgot and change password routes

--- client/src/Routes.js
+++ client/src/Routes.js
@@ -4,6 +4,8 @@ import { Switch } from "react-router-dom";
 import Home from "./pages/Home";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
+import ForgotPassword from "./pages/ForgotPassword";
+import ChangePassword from "./pages/ChangePassword";
 import Action from "./pages/Action";
 
 import FancyRoute from "./components/FancyRoute";
@@ -15,6 +17,12 @@ class Routes extends Component {
         <FancyRoute exact path="/" component={Home} />
         <FancyRoute exact path="/register" component={Register} />
         <FancyRoute exact path="/login" component={Login} />
+        <FancyRoute exact path="/forgot-password" component={ForgotPassword} />
+        <FancyRoute
+          exact
+          path="/change-password/:key"
+          component={ChangePassword}
+        />
         <FancyRoute path="/t" component={Action} />
       </Switch>
     );
