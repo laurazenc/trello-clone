aedf95620df43b921f2a109a85d5faa5a2341d70
Laura <laurazenc@gmail.com>
2018-11-15 08:52:11 +0100

    Schemas validations
    Need a better way to manage schemas between the server and the client

---
+++ client/src/utils/validations/messages/userMessages.js
@@ -0,0 +1,11 @@
+export const userMessages = {
+  emailNotLongEnough: "Email must be at least 3 characters long",
+  passwordNotLongEnough: "Password must be at least 3 characters long",
+  displayNameNotLongEnough: "First name must be at least 3 characters long",
+  invalidEmail: "Email must be a valid email",
+  accountLocked: "Account is locked",
+  emailNotConfirmed: "Email not confirmed",
+  confirmPasswordDoesntMatch: "Confirm password doesn't match",
+  emailAlreadyExists: "Email already exists",
+  invalidLogin: "Invalid login"
+};
