ab22bc5f6aa58cb67f71c669063f2bf2c66c6862
Laura <laurazenc@gmail.com>
2018-11-16 14:52:46 +0100

    Me query

--- server/src/modules/user/resolver.js
+++ server/src/modules/user/resolver.js
@@ -1,8 +1,12 @@
 import { register } from "./register";
 import { login } from "./login";
+import { me } from "./me";
 import { sendForgotPasswordEmail, changePassword } from "./forgotPassword";
 
 export const resolvers = {
+  Query: {
+    me
+  },
   Mutation: {
     register,
     login,
