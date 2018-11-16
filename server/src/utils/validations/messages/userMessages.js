0b2d5a3dd9cb356777e36ccd44646ece9aaeeb81
Laura <laurazenc@gmail.com>
2018-11-16 13:10:20 +0100

    Validation schemas and error messages

--- server/src/utils/validations/messages/userMessages.js
+++ server/src/utils/validations/messages/userMessages.js
@@ -1,7 +1,7 @@
 export const userMessages = {
   emailNotLongEnough: "Email must be at least 3 characters long",
   passwordNotLongEnough: "Password must be at least 3 characters long",
-  displayNameNotLongEnough: "First name must be at least 3 characters long",
+  displayNameNotLongEnough: "Display name must be at least 3 characters long",
   invalidEmail: "Email must be a valid email",
   accountLocked: "Account is locked",
   emailNotConfirmed: "Email not confirmed",
