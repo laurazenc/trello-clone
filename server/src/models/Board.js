b893eed7f15dec973ce97cac7323796cf41e32f1
Laura <laurazenc@gmail.com>
2018-11-17 17:49:38 +0100

    Handles types for mongodb ids and transform it into strings

--- server/src/models/Board.js
+++ server/src/models/Board.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-const validBoardSchema = new mongoose.Schema(
+const boardSchema = new mongoose.Schema(
   {
     name: { type: String, required: true },
     owner: { type: mongoose.Types.ObjectId, ref: "User" }
@@ -8,4 +8,9 @@ const validBoardSchema = new mongoose.Schema(
   { timestamps: true }
 );
 
-export default mongoose.model("Board", validBoardSchema);
+export default mongoose.model("Board", boardSchema);
+
+const { ObjectId } = mongoose.Types;
+ObjectId.prototype.valueOf = function() {
+  return this.toString();
+};
